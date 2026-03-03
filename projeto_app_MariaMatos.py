2708a5f0ee25cd6f9479d9b250ef06f510e983d6
mariamatosemvocacao <mariaeduardadesouzamatos2@gmail.com>
2026-03-03 09:51:30 -0300

    Update- Criaçao de menu Hamburgueria
    Criaçao de menu da Hamburgueria 
    Acesso ao github

---
+++ projeto_app_MariaMatos.py
@@ -0,0 +1,24 @@
+'''
+CRUD
+
+
+Hamburguer
+
+print('Ola Mundo! - Quebrei essa maldiçao') #O print exibe na tela
+
+
+input('Pressione Enter para sair')
+#input serve para pedir da tela
+'''
+
+print('\nHamburgueria!\n')
+
+print('1. Cardapio')
+print('2. Delivery')
+print('0. Sair')
+
+while True: 
+
+    escolha_menu == input ('\nEscolha uma opção: ')
+
+    if escolha_menu == input ('\n Escolha uma opçao: ')
